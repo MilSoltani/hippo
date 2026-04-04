a6333a475f2667ca1b36b024ea4741c2dc7ef267
MilSoltani <miladsoltani@outlook.de>
2026-04-04 15:19:58 +0200

    installed antfu eslint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,7 +9,8 @@ app.get('/', (c) => {
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
 }, (info) => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${info.port}`)
 })
