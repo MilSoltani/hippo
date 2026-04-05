d432f677bce6b51c8e0dff36a2cf1ae7f9c160f9
MilSoltani <miladsoltani@outlook.de>
2026-04-05 09:20:43 +0200

    installed vitest

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    projects: [
+      {
+        extends: true,
+        test: {
+          include: ['**/*.unit.test.ts'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@api': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
