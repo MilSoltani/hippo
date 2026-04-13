80c55ccc7a655e032b3f447e78289ae540cfea3a
MilSoltani <miladsoltani@outlook.de>
2026-04-13 20:37:39 +0200

    feat: openapi doc and swagger ui

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,34 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { swaggerUI } from '@hono/swagger-ui'
+import { OpenAPIHono } from '@hono/zod-openapi'
 import { cors } from 'hono/cors'
+import { HTTPException } from 'hono/http-exception'
 import { logger } from 'hono/logger'
 import { ticketModule, userModule } from './app'
 
-const server = new Hono()
-  .use(cors())
+const server = new OpenAPIHono()
+  .doc('/doc', {
+    openapi: '3.0.0',
+    info: {
+      title: 'hippo API',
+      version: '1.0.0',
+    },
+  })
+  .use('/*', cors())
   .use(logger())
   .route('/users', userModule.handler)
   .route('/tickets', ticketModule.handler)
+  .notFound((c) => {
+    return c.json({ error: 'Not Found!' }, 404)
+  })
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return err.getResponse()
+    }
+    console.error(err)
+    return c.text('Internal Server Error', 500)
+  })
+  .get('/ui', swaggerUI({ url: '/doc' }))
 
 serve({
   fetch: server.fetch,
